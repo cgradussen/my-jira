d90b02b787c41c5b286285fa8a37f1cdb2f44065
Carlo Gradussen <info@xipas.com>
2024-02-04 15:04:41 +0100

    Initial MY-Jira
    Initial working my-Jira CLI. Needs refactoring and additional tests.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,9 +1,3 @@
-//mod my_jira::db;
-//mod models;
-//use crate::models::DBState;
-
-//use models::JiraDatabase;
-
 // #########.#########.#########.#########.#########.#########.#########.#########.#########.#########.
 #[cfg(test)]
 mod tests {}
